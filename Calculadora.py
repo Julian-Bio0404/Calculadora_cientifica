3bfdd5a6cff2805812853d5e1f5f86e7a887894a
Julia <juliancamilohermida@hotmail.com>
2021-02-25 14:43:29 -0500

    funcion multiplicar y dividir añadidas, valores int

--- Calculadora.py
+++ Calculadora.py
@@ -1,16 +1,13 @@
-from tkinter import *
+from tkinter import Frame, Entry, Tk, Button, StringVar
 
 root = Tk()
-
 frame1 = Frame(root)
 frame1.pack()
 operacion = ""
-resultado = 0.0
+
+resultado = 0
 reset_pantalla = False
-
-num1=0.0
-contador_resta=0
-
+num1 = 0
 
 #---------------------pantalla------------------------------#
 numero_pantalla = StringVar()
@@ -18,7 +15,7 @@ pantalla = Entry(frame1, textvariable=numero_pantalla)
 pantalla.grid(row=1, column=1, padx=10, pady=10, columnspan=4)
 pantalla.config(background="black", fg="#03f943", justify="right")
 
-#--------------------Pulsación_teclado-------------------#
+#--------------------Pulsación_teclado--------------------#
 def numeroPulsado(num):
     global reset_pantalla
 
@@ -33,12 +30,13 @@ def suma(num):
     global operacion
     global resultado
     global reset_pantalla
-    resultado += float(num)
+    resultado += int(num)
     operacion = "suma"
     reset_pantalla = True
     numero_pantalla.set(resultado)
 
 #--------------------funcion resta--------------------------#
+contador_resta = 0
 def resta(num):
     global operacion
     global resultado
@@ -46,33 +44,90 @@ def resta(num):
     global contador_resta
     global num1
     
-    if contador_resta==0:
-        num1 = float(num)
+    if contador_resta == 0:
+        num1 = int(num)
         resultado = num1
     else:
         if contador_resta == 1:
-            resultado = num1 - float(num)
+            resultado = num1 - int(num)
         else:
-            resultado = float(resultado) - float(num)
+            resultado = int(resultado) - int(num)
         numero_pantalla.set(resultado)
         resultado = numero_pantalla.get()
     contador_resta = contador_resta + 1
     operacion = "resta"
     reset_pantalla = True
 
+#-----------------funcion multiplicacion---------------------#
+contador_multiplicar = 0
+def multiplicar(num):
+    global operacion
+    global resultado
+    global reset_pantalla
+    global contador_multiplicar
+    global num1
+
+    if contador_multiplicar == 0:
+        num1 = int(num)
+        resultado = num1
+    else:
+        if contador_multiplicar == 1:
+            resultado = num1 * int(num)
+        else:
+            resultado = int(resultado) * int(num)
+        numero_pantalla.set(resultado)
+        resultado = numero_pantalla.get()
+    contador_multiplicar += 1
+    operacion ="multiplicacion"
+    reset_pantalla = True
+
+#------------------funcion dividir--------------------------------#
+contador_dividir = 0
+def dividir(num):
+    global operacion
+    global resultado
+    global reset_pantalla
+    global contador_dividir
+    global num1
+
+    if contador_dividir == 0:
+        num1 = float(num)
+        resultado = num1
+    else:
+        if contador_dividir == 1:
+            resultado = num1 / float(num)
+        else:
+            resultado = float(resultado) / float(num)
+        numero_pantalla.set(resultado)
+        resultado = numero_pantalla.get()
+    contador_dividir += 1
+    operacion = "division"
+    reset_pantalla = True
+
+
 #------------------funcion que muestra el resultado total---------------------#
 def el_resultado():
     global operacion
     global resultado
     global contador_resta
+    global contador_multiplicar
+    global contador_dividir
 
     if operacion == "suma":
-        numero_pantalla.set(float(resultado) + float(numero_pantalla.get()))
+        numero_pantalla.set(resultado + int(numero_pantalla.get()))
         resultado = 0
     elif operacion == "resta":
-        numero_pantalla.set(float(resultado) - float(numero_pantalla.get()))
+        numero_pantalla.set(int(resultado) - int(numero_pantalla.get()))
         contador_resta = 0
         resultado = 0
+    elif operacion == "multiplicacion":
+        numero_pantalla.set(int(resultado) * int(numero_pantalla.get()))
+        resultado = 0
+        contador_multiplicar = 0
+    elif operacion == "division":
+        numero_pantalla.set(int(resultado) / int(numero_pantalla.get()))
+        resultado = 0
+        contador_dividir = 0
 
 #-----------------------------botones fila1----------------------------------#
 boton7 = Button(frame1, text="7", width=3, command=lambda:numeroPulsado("7"))
@@ -81,7 +136,7 @@ boton8 = Button(frame1, text="8", width=3, command=lambda:numeroPulsado("8"))
 boton8.grid(row=2, column=2)
 boton9 = Button(frame1, text="9", width=3, command=lambda:numeroPulsado("9"))
 boton9.grid(row=2, column=3)
-botonDiv = Button(frame1, text="/", width=3)
+botonDiv = Button(frame1, text="/", width=3, command=lambda:dividir(numero_pantalla.get()))
 botonDiv.grid(row=2, column=4)
 
 #-----------------------------botones fila2----------------------------------#
@@ -91,7 +146,7 @@ boton5 = Button(frame1, text="5", width=3, command=lambda:numeroPulsado("5"))
 boton5.grid(row=3, column=2)
 boton6 = Button(frame1, text="6", width=3, command=lambda:numeroPulsado("6"))
 boton6.grid(row=3, column=3)
-botonX = Button(frame1, text="X", width=3)
+botonX = Button(frame1, text="X", width=3, command=lambda:multiplicar(numero_pantalla.get()))
 botonX.grid(row=3, column=4)
 
 #-----------------------------botones fila3----------------------------------#
